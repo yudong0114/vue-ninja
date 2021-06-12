0b3c6745b3fc8d5597b51ea27cff0ad6ddc34eba
yudong0114 <yukisaito114@gmail.com>
2021-06-12 22:23:17 +0900

    [update]Q5m

--- ninja-code/src/App.vue
+++ ninja-code/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+/*<template>
   <div id="app">
     <ninjacode text="Hello NinjaCode!">
   </ninjacode></div>
@@ -13,4 +13,41 @@ export default {
     ninjacode
   }
 }
+</script>*/
+
+
+
+<template>
+  <div id="app">
+    <sidebar></sidebar>
+    <Main></Main>
+  </div>
+</template>
+<script>
+import Main from './components/Main.vue'
+import sidebar from './components/Sidebar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main,
+    sidebar: sidebar
+  }
+}
 </script>
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  min-height: 100vh;
+}
+</style>
---
+++ ninja-code/src/components/Sidebar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="sidebar-container">
+    <h1>Sidebar.vue</h1>
+  </div>
+</template>
+<style>
+  .sidebar-container {
+    width: 20%;
+    background: #fff;
+  }
+  .sidebar-container h1 {
+    padding-top: 50px;
+  }
+</style>
